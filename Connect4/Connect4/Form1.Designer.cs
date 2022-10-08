e1167aec5899d4f1611f32f5e197466ec3c1197f
Robert Dinca <71851178+robertDinca2003@users.noreply.github.com>
2022-10-08 12:25:51 +0300

    Add files via upload

---
+++ Connect4/Connect4/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Connect4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGameName = new System.Windows.Forms.Label();
+            this.btn_play = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.panelGame = new System.Windows.Forms.Panel();
+            this.lblCurrentTurn = new System.Windows.Forms.Label();
+            this.btn_back = new System.Windows.Forms.Button();
+            this.panelGame.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblGameName
+            // 
+            this.lblGameName.AutoSize = true;
+            this.lblGameName.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGameName.ForeColor = System.Drawing.Color.DarkGreen;
+            this.lblGameName.Location = new System.Drawing.Point(282, 5);
+            this.lblGameName.Name = "lblGameName";
+            this.lblGameName.Size = new System.Drawing.Size(462, 108);
+            this.lblGameName.TabIndex = 0;
+            this.lblGameName.Text = "Connect4";
+            // 
+            // btn_play
+            // 
+            this.btn_play.BackColor = System.Drawing.Color.DarkGreen;
+            this.btn_play.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_play.ForeColor = System.Drawing.Color.PapayaWhip;
+            this.btn_play.Location = new System.Drawing.Point(312, 325);
+            this.btn_play.Name = "btn_play";
+            this.btn_play.Size = new System.Drawing.Size(397, 90);
+            this.btn_play.TabIndex = 1;
+            this.btn_play.Text = "PLAY";
+            this.btn_play.UseVisualStyleBackColor = false;
+            this.btn_play.Click += new System.EventHandler(this.btn_play_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.BackColor = System.Drawing.Color.DarkGreen;
+            this.btn_exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_exit.ForeColor = System.Drawing.Color.PapayaWhip;
+            this.btn_exit.Location = new System.Drawing.Point(312, 461);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(397, 90);
+            this.btn_exit.TabIndex = 2;
+            this.btn_exit.Text = "EXIT";
+            this.btn_exit.UseVisualStyleBackColor = false;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // panelGame
+            // 
+            this.panelGame.Controls.Add(this.btn_back);
+            this.panelGame.Controls.Add(this.lblCurrentTurn);
+            this.panelGame.Controls.Add(this.btn_exit);
+            this.panelGame.Controls.Add(this.btn_play);
+            this.panelGame.Controls.Add(this.lblGameName);
+            this.panelGame.Location = new System.Drawing.Point(1, 4);
+            this.panelGame.Name = "panelGame";
+            this.panelGame.Size = new System.Drawing.Size(1060, 1026);
+            this.panelGame.TabIndex = 3;
+            this.panelGame.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panelGame_MouseClick);
+            // 
+            // lblCurrentTurn
+            // 
+            this.lblCurrentTurn.AutoSize = true;
+            this.lblCurrentTurn.Enabled = false;
+            this.lblCurrentTurn.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrentTurn.ForeColor = System.Drawing.Color.DarkGreen;
+            this.lblCurrentTurn.Location = new System.Drawing.Point(50, 159);
+            this.lblCurrentTurn.Name = "lblCurrentTurn";
+            this.lblCurrentTurn.Size = new System.Drawing.Size(333, 59);
+            this.lblCurrentTurn.TabIndex = 3;
+            this.lblCurrentTurn.Text = "Current Turn:";
+            this.lblCurrentTurn.Visible = false;
+            // 
+            // btn_back
+            // 
+            this.btn_back.BackColor = System.Drawing.Color.DarkGreen;
+            this.btn_back.Enabled = false;
+            this.btn_back.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_back.ForeColor = System.Drawing.Color.PapayaWhip;
+            this.btn_back.Location = new System.Drawing.Point(786, 23);
+            this.btn_back.Name = "btn_back";
+            this.btn_back.Size = new System.Drawing.Size(259, 90);
+            this.btn_back.TabIndex = 4;
+            this.btn_back.Text = "Back";
+            this.btn_back.UseVisualStyleBackColor = false;
+            this.btn_back.Visible = false;
+            this.btn_back.Click += new System.EventHandler(this.btn_back_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PapayaWhip;
+            this.ClientSize = new System.Drawing.Size(1058, 1024);
+            this.Controls.Add(this.panelGame);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Connect4";
+            this.panelGame.ResumeLayout(false);
+            this.panelGame.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGameName;
+        private System.Windows.Forms.Button btn_play;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Panel panelGame;
+        private System.Windows.Forms.Label lblCurrentTurn;
+        private System.Windows.Forms.Button btn_back;
+    }
+}
+
---
+++ Connect4/Connect4/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Connect4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Graphics g;
+
+        int[,] connect = new int[7,6];
+
+        int currentTurn ;
+
+        bool gameEnd = true ;
+
+        int total;
+
+        void win()
+        {
+            if (total != 46)
+                lblCurrentTurn.Text = "Player " + currentTurn.ToString() + " won!";
+            else
+                lblCurrentTurn.Text = "Draw!";
+            gameEnd = true;
+        }
+
+        void verif()
+        {
+            if (total == 48) {win(); return; }
+            bool winner;
+            for(int i = 0; i < 6; i++)
+                for(int j = 5; j >= 0; j--)
+                {
+                    winner = true;
+                    //MessageBox.Show(i.ToString() + "  " + j.ToString());
+                    for (int k = 0; k <= 3; k++)
+                         if (i+k > 6 ||connect[i + k, j] != currentTurn ) winner = false;
+                    if (winner) { win(); return; }
+
+                    winner = true;
+
+                    for (int k = 0; k <= 3; k++)
+                        if (j + k > 5 || connect[i, j + k] != currentTurn) winner = false;
+                    if(winner) { win(); return; }
+
+                    winner = true;
+
+                    for (int k = 0; k <= 3; k++)
+                        if (j + k > 5 || i+k>6|| connect[i+k, j + k] != currentTurn) winner = false;
+                    if (winner) { win(); return; }
+
+                    winner = true;
+
+                    for (int k = 0; k <= 3; k++)
+                        if (j - k < 0 || i + k > 6 || connect[i + k, j - k] != currentTurn) winner = false;
+                    if (winner) { win(); return; }
+                }
+        }
+
+        void MakeMove(int x ,int y)
+        {
+            
+            if (x < 0 || y < 0 ) return;
+            x = x / 70;
+            y = y / 70; 
+            if (x > 6) return;
+            //MessageBox.Show(x.ToString() + "  " + y.ToString());
+            for (y = 5; y >= 0; y--) if (connect[x, y] == 0) break;
+            if (y < 0) return;
+            connect[x, y] = currentTurn;
+           // MessageBox.Show(connect[x,y].ToString() );
+            if (currentTurn == 1)
+                g.FillEllipse(new SolidBrush(Color.Red), 100 + x * 70, 200 + y * 70, 60, 60);
+            else
+                g.FillEllipse(new SolidBrush(Color.Yellow), 100 + x * 70, 200 + y * 70, 60, 60);
+            verif();
+            if (gameEnd) return;
+            if (currentTurn == 1) currentTurn = 2;
+            else currentTurn = 1;
+            lblCurrentTurn.Text = "Current Turn: " + currentTurn.ToString();
+            total++;
+
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_play_Click(object sender, EventArgs e)
+        {
+            gameEnd = false;
+
+            btn_exit.Visible = false;
+            btn_exit.Enabled = false;
+
+            btn_play.Enabled = false;
+            btn_play.Visible = false;
+
+            btn_back.Visible = true;
+            btn_back.Enabled = true;
+
+            lblCurrentTurn.Visible = true;
+
+            total = 0;
+
+            for (int i = 0; i <= 6; i++)
+                for (int j = 0; j <= 5; j++)
+                    connect[i, j] = 0;
+
+            g = panelGame.CreateGraphics();
+
+            g.FillRectangle(new SolidBrush(Color.SteelBlue), 70, 170, 540, 460);
+            for(int i = 0; i < 6; i++ )
+                for(int j = 0; j < 7; j++)
+                {
+                    g.FillEllipse(new SolidBrush(Color.PapayaWhip), 100 + j * 70, 200 + i * 70, 60, 60);
+                }
+            currentTurn = 1;
+            lblCurrentTurn.Text = "Current Turn: " + currentTurn.ToString();
+        }
+
+        private void panelGame_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (gameEnd) return;
+            int x = e.X- 100;
+            int y = e.Y - 200;
+            MakeMove(x, y);
+        }
+
+        private void btn_back_Click(object sender, EventArgs e)
+        {
+            gameEnd = true;
+
+            g.Clear(Color.PapayaWhip);
+
+            for (int i = 0; i <= 6; i++)
+                for (int j = 0; j <= 5; j++)
+                    connect[i, j] = 0;
+
+            lblCurrentTurn.Visible = false;
+
+            btn_exit.Visible = true;
+            btn_exit.Enabled = true;
+
+            btn_play.Enabled = true;
+            btn_play.Visible = true;
+
+            btn_back.Visible = false;
+            btn_back.Enabled = false;
+
+            
+        }
+    }
+}
